05011c8213288c1ad4f4ab481575abc0d71af0be
Nithesh Balakrishnan <antechnologiesindia@gmail.com>
2023-09-06 16:43:37 +0530

    Updated script

--- scripts/startup_script.sh
+++ scripts/startup_script.sh
@@ -1,58 +1,56 @@
 #!/bin/bash
-# Create a dummy file in /tmp folder
-# touch /tmp/my_dummy_file.txt
 
-# # Install essential build tools (if not already installed)
-# echo "Installing build tools..."
-# sudo apt-get update
-# sudo apt-get install -y build-essential
+# Variables
+DECIDIM_VERSION="0.22.0" # Change this to the desired Decidim version
+DECIDIM_APP_NAME="my-decidim-app" # Change this to your app's name
+DB_PASSWORD="your_password" # Change this to your database password
+SECRET_KEY_BASE="your_secret_key_base" # Change this to a secret key base for Decidim
 
-# # Download Vim source code
-# echo "Downloading Vim source code..."
-# wget -O /tmp/vim-source.tar.gz https://github.com/vim/vim/archive/master.tar.gz
+# Function to install Docker and Docker Compose
+install_docker_and_compose() {
+  # Add Docker repository
+  sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
 
-# # Extract Vim source code
-# echo "Extracting Vim source code..."
-# tar -zxvf /tmp/vim-source.tar.gz -C /tmp/
+  # List available repositories
+  sudo dnf repolist -v
 
-# # Navigate to the Vim source directory
-# cd /tmp/vim-master
+  # Install Docker (latest version)
+  sudo dnf install --nobest docker-ce
 
-# # Configure and compile Vim
-# echo "Configuring and compiling Vim..."
-# ./configure
-# make
-# sudo make install
+  # Install containerd.io
+  sudo dnf install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
 
-# # Verify Vim installation
-# vim --version
+  # Install Docker Compose
+  curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o docker-compose
+  sudo mv docker-compose /usr/local/bin && sudo chmod +x /usr/local/bin/docker-compose
+  sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
-# # Clean up
-# rm /tmp/my_dummy_file.txt
+  # Enable and start Docker service
+  sudo systemctl enable --now docker
 
-# Check if Docker is installed and install it if necessary
-if ! command -v docker &> /dev/null; then
-    echo "Docker not found. Installing Docker..."
-    curl -fsSL https://get.docker.com -o get-docker.sh
-    sudo sh get-docker.sh
-    sudo usermod -aG docker $USER
-    rm get-docker.sh
-    echo "Docker installed."
-fi
+  sudo chmod 666 /var/run/docker.sock
 
-# Check if Docker Compose is installed and install it if necessary
-if ! command -v docker-compose &> /dev/null; then
-    echo "Docker Compose not found. Installing Docker Compose..."
-    sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
-    echo "Docker Compose installed."
-fi
+  # Check if Docker is active
+  if systemctl is-active docker >/dev/null; then
+    echo "Docker is active."
+  else
+    echo "Docker is not active."
+  fi
+}
 
-# Clone the repository
-git clone https://github.com/decidim/docker.git
+# Install Git
+sudo dnf install git -y
 
-# Change directory to the cloned folder
-cd docker
+# Install Docker and Docker Compose
+install_docker_and_compose
 
-# Run docker-compose up in detached mode
-/usr/local/bin/docker-compose up -d
+# Clone Decidim repository
+git clone --branch v$DECIDIM_VERSION https://github.com/decidim/decidim.git $DECIDIM_APP_NAME
+
+# Navigate to the Decidim app directory
+cd $DECIDIM_APP_NAME
+
+# Start Decidim using Docker Compose
+docker-compose up -d
+
+echo "Decidim is now installed and running at http://localhost:3000"
