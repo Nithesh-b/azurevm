712a01d86ccb357243ff2cb96443d852eb22d1b4
Nithesh Balakrishnan <antechnologiesindia@gmail.com>
2023-09-06 12:41:49 +0530

    Updated vnet

--- vnet.tf
+++ vnet.tf
@@ -31,3 +31,17 @@ resource "azurerm_network_security_rule" "ssh" {
   resource_group_name         = azurerm_resource_group.example.name
   network_security_group_name = azurerm_network_security_group.example.name
 }
+
+resource "azurerm_network_security_rule" "app" {
+  name                        = "AppRule"
+  priority                    = 1002
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "3000"
+  source_address_prefix       = "Internet"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.example.name
+  network_security_group_name = azurerm_network_security_group.example.name
+}
